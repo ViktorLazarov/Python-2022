2a37bd4fdb4a3dad79f2a74bad91ca29a3991be1
Viktor Lazarov <viktorlazarov@abv.bg>
2023-09-15 19:06:43 +0200

    .

--- bank_account.py
+++ bank_account.py
@@ -1,18 +1,19 @@
 class BankAccount:
-    def __init__(self,owner):
+    def __init__(self, owner):
         self.owner = owner
         self.balance = 0.0
 
-    def getBalance(self):
+    def get_balance(self):
         return self.balance
 
-    def deposit(self,deposit_amount):
+    def deposit(self, deposit_amount):
         self.balance += deposit_amount
         return self.balance
 
-    def withdraw(self,withdraw_amount):
+    def withdraw(self, withdraw_amount):
         self.balance -= withdraw_amount
         return self.balance
+
 
 acct = BankAccount("Darcy")
 print(acct.owner)
